4145d6a6b06e920f025286b511e96bc0527ff9ac
Alert <zhanglin-pc@live.com>
2015-08-14 10:15:06 +0800

    footer

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+<footer id="bottom"><!-- #footer start -->
+    <div class="content">
+        <div class="inner">
+            <div class="foot-warp">
+                <span class="links">Links:
+                    <a href="#" rel="external" target="_blank">Links</a>
+                    <a href="#" rel="external" target="_blank">Links</a>
+                    <a href="#" rel="external">待添加</a>
+                </span><br/>
+                        <img class="emoji" draggable="false" alt="©" src="chrome-extension://bmimjhihamhbmappdomnmlpnapggcnef/svg/a9.svg"> 2015 Ciyuan'ai / Theme by
+                        <a href="http://ciyuanai.net" target="_blank">Axhello</a> /
+                        <a href="#" target="_blank">站点地图</a>
+                </span>
+            </div>
+        </div>
+    </div>
+</footer><!-- #footer end -->
+</div><!-- #wrapper end -->
+</body>
+</html>
